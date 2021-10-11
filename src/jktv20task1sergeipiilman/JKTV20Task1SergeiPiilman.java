6acfbb65a46415662bd657933e2df1c43460053b
pupil <pupil@ivkhk.ee>
2021-10-11 15:31:00 +0300

    Первое задание

---
+++ src/jktv20task1sergeipiilman/JKTV20Task1SergeiPiilman.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jktv20task1sergeipiilman;
+
+/**
+ *
+ * @author pupil
+ */
+public class JKTV20Task1SergeiPiilman {
+
+
+       public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+    
+}
